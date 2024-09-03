062a1ee2869bac6b8fede52ce85aec477c629056
Roaa Almasoud <roaa.almasoud@wellsitesoftware.com>
2024-09-03 20:21:27 +0200

    fix issues

--- src/components/common/formComponent.jsx
+++ src/components/common/formComponent.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-import _ from 'lodash';
-import Utils from '../utils/utils';
-import { PhoneInput } from 'react-international-phone';
-import Multiselect from 'multiselect-react-dropdown';
-import Select from 'react-dropdown-select';
-import DatePicker from 'react-date-picker';
-import 'react-date-picker/dist/DatePicker.css';
-import 'react-calendar/dist/Calendar.css';
-import 'react-international-phone/style.css';
-import { PhoneNumberUtil } from 'google-libphonenumber';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
+import _ from "lodash";
+import Utils from "../utils/utils";
+import { PhoneInput } from "react-international-phone";
+import Multiselect from "multiselect-react-dropdown";
+import Select from "react-dropdown-select";
+import DatePicker from "react-date-picker";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
+import "react-international-phone/style.css";
+import { PhoneNumberUtil } from "google-libphonenumber";
 
 function FormComponent({
   fields,
@@ -22,13 +22,13 @@ function FormComponent({
   next,
   userList,
   saveSectionForm,
-  type = '',
+  type = "",
 }) {
   const [validated, setValidated] = useState(false);
   const [form, setForm] = useState(formData);
   const [errors, setErrors] = useState({});
-  const [errorMessage, setErrorMessage] = useState('');
-  const [selectedItems, setSelectedItems] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [selectedItems, setSelectedItems] = useState(userList);
   const phoneUtil = PhoneNumberUtil.getInstance();
   const isPhoneValid = (phone) => {
     if (phone)
@@ -54,7 +54,7 @@ function FormComponent({
       let field_conf = fields.find((f) => f.name === field);
 
       if (!Utils.isEmpty(field_conf?.regex)) {
-        if (field_conf.regex === 'phone') {
+        if (field_conf.regex === "phone") {
           if (!isPhoneValid(form[field]))
             errorsObject[field] = `Invalid ${field_conf.label}`;
           else delete errorsObject[field];
@@ -62,7 +62,7 @@ function FormComponent({
           if (!Utils.regexCheck(form[field], field_conf.regex))
             errorsObject[field] = `Invalid ${field_conf.label}`;
         }
-      } else if (typeof form[field] === 'object') {
+      } else if (typeof form[field] === "object") {
         if (!Utils.isEmpty(form[field])) {
           delete errorsObject[field];
         }
@@ -86,14 +86,21 @@ function FormComponent({
     }
 
     setErrors(errorsObject);
-    if (typeof event === 'object') {
+    if (typeof event === "object") {
       event?.preventDefault();
       event?.stopPropagation();
     }
   };
 
-  const updateForm = (e, field, value, type = 'text') => {
+  const updateForm = (e, field, value, type = "text") => {
     //form[field] = type === 'number' ? parseFloat(value) : value;
+    if (type === "date") {
+      value = `${value.getFullYear()}-${
+        value.getMonth() + 1 <= 9
+          ? `0${value.getMonth() + 1}`
+          : value.getMonth() + 1
+      }-${value.getDate() <= 9 ? `0${value.getDate()}` : value.getDate()}`;
+    }
     form[field] = value;
     validate(e, field);
     setForm({ ...form });
@@ -109,7 +116,7 @@ function FormComponent({
 
   const changeList = (values, field) => {
     let value = values[0];
-    if (field === 'language') {
+    if (field === "language") {
       form.id = value.id;
       form.name = value.name;
       setForm({ ...form });
@@ -123,9 +130,9 @@ function FormComponent({
     if (field) {
       let fieldInput = null;
       switch (field.type) {
-        case 'text':
-        case 'password':
-        case 'number':
+        case "text":
+        case "password":
+        case "number":
           fieldInput = (
             <div>
               <Form.Control
@@ -140,13 +147,13 @@ function FormComponent({
           );
           break;
 
-        case 'phone':
+        case "phone":
           fieldInput = (
             <div>
               <PhoneInput
                 className="number"
-                country={'us'}
-                value={form[field.name] ? form[field.name] : ''}
+                country={"us"}
+                value={form[field.name] ? form[field.name] : ""}
                 // onChange={(value, country, event, formattedValue) =>
                 //   updateForm(event, field.name, formattedValue)
                 // }
@@ -161,7 +168,7 @@ function FormComponent({
           );
           break;
 
-        case 'multiSelect':
+        case "multiSelect":
           fieldInput = (
             <div>
               <Multiselect
@@ -174,21 +181,21 @@ function FormComponent({
             </div>
           );
           break;
-        case 'list':
+        case "list":
           fieldInput = (
             <div>
               <Select
                 options={field.options}
-                labelField={'name'}
+                labelField={"name"}
                 valueField="id"
-                values={field.name === 'language' ? [form] : [form[field.name]]}
+                values={field.name === "language" ? [form] : [form[field.name]]}
                 onChange={(values) => changeList(values, field.name)}
               />
             </div>
           );
           break;
 
-        case 'staticList':
+        case "staticList":
           fieldInput = (
             <div>
               <Form.Select
@@ -205,11 +212,11 @@ function FormComponent({
           );
           break;
 
-        case 'radio':
+        case "radio":
           fieldInput = (
             <div>
               <Form.Check
-                type={'checkbox'}
+                type={"checkbox"}
                 label={field.label}
                 onChange={(e) => updateForm(e, field.name, !form[field.name])}
                 checked={form[field.name]}
@@ -217,14 +224,14 @@ function FormComponent({
             </div>
           );
           break;
-        case 'date':
+        case "date":
           fieldInput = (
             <div>
               <DatePicker
                 selected={form[field.name]}
                 value={form[field.name]}
-                onChange={(date, e) => updateForm(e, field.name, date)}
-                format={'yyyy-MM-dd'}
+                onChange={(date, e) => updateForm(e, field.name, date, "date")}
+                format={"yyyy-MM-dd"}
               />
             </div>
           );
@@ -234,9 +241,9 @@ function FormComponent({
       let returnedFieldWrapper = (
         <div className="mb-4">
           <Form.Label>
-            {`${field.label} ${field.optional ? '' : '*'}`}
+            {`${field.label} ${field.optional ? "" : "*"}`}
           </Form.Label>
-          <div className={'k-form-field-wrap'}>
+          <div className={"k-form-field-wrap"}>
             {fieldInput}
             {errors[field.name] && (
               <Form.Control.Feedback type="invalid" className="mb-3">
@@ -257,14 +264,14 @@ function FormComponent({
         })}
       <div className="form-action">
         <button
-          className={`${next ? 'nav-button' : 'underline-button'}`}
+          className={`${next ? "nav-button" : "underline-button"}`}
           onClick={(e) => validate(e)}
         >
-          {`${type === 'new' ? 'Add' : 'Save'}`}
+          {`${type === "new" ? "Add" : "Save"}`}
         </button>
         {next && (
           <button
-            className={'nav-button-border'}
+            className={"nav-button-border"}
             onClick={(e) => {
               e.preventDefault();
               e.stopPropagation();
